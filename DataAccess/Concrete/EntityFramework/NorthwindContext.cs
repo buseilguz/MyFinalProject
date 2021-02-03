83e36702ea58e0a0641f7a2b3bf1cef32db46b8d
buseilguz <buse.ilguz@gmail.com>
2021-02-03 23:56:07 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Db tabloları ile proje claaslarını ilişkilendirme
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocalDB;Database=Northwind;Trusted_Connection=true");
+            
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category  > Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+
+    }
+}
